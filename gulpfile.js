c0d0f0a57ac028d893efb6d8a3d7a80695c27f5d
Péter Szrnka <szrnka.peter@gmail.com>
2017-12-07 13:17:12 +0100

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,19 @@
+var gulp = require('gulp');
+var karma = require('gulp-karma');
 
+var allFiles = [
+ 'app/plugins/angular.js',
+ 'src/*.js',
+ 'test/*.js'
+];
+gulp.task('test', function(coverage) {
+  gulp.src(allFiles)
+  .pipe(karma({
+  configFile: 'karma.conf.js',
+  action: 'run'
+}))
+.on('error', function(err) {
+ // Make sure failed tests cause gulp to exit non-zero
+   throw err;
+ });
+});
