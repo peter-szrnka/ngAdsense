36da5247450b2e27b8c908017c67cf5b1977487c
Péter Szrnka <szrnka.peter@gmail.com>
2017-12-07 14:37:08 +0100

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 describe('Unit testing of directive', function() {
   var $compile,
-      $rootScope;
+      $rootScope, element;
 
   // Load the myApp module, which contains the directive
   beforeEach(module('ngAdsense'));
@@ -11,6 +11,15 @@ describe('Unit testing of directive', function() {
     // The injector unwraps the underscores (_) from around the parameter names when matching
     $compile = _$compile_;
     $rootScope = _$rootScope_;
+    scope = $rootScope.$new();
+    element = '<adsense-directive '+
+			'ad-client="YOUR_CLIENT_ID" '+
+			'ad-slot="YOUR_SLOT_ID"  '+
+			'ad-format="auto" '+
+			'css-style="display:inline-block;width:320px;height:50px;"> '+
+		'</adsense-directive>';
+    element = $compile(element)(scope);
+    scope.$digest();
   }));
   
   // TODO finish
