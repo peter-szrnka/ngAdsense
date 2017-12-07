7ad16eb961d430eb44cc35f38d21f336ab1b15b0
Péter Szrnka <szrnka.peter@gmail.com>
2017-12-07 14:41:47 +0100

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -3,7 +3,6 @@ module.exports = function(config) {
  config.set({
  frameworks: ['jasmine'],
   files: [
-  'node_modules/angular/angular.js', 
   'node_modules/angular/angular.min.js',
   'node_modules/angular-mocks/angular-mocks.js',
   'app/*.js', 
